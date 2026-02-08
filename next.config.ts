af36a1145be2b16664b4387a10cb3d5a7b125691
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2026-02-08 17:47:35 +0530

    fix: vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: [
+    "@coinbase/cdp-sdk",
+    "@base-org/account",
+    "@solana/kit",
+    "@solana-program/system",
+    "@solana-program/token",
+    "axios",
+    "axios-retry",
+  ],
   turbopack: {},
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
